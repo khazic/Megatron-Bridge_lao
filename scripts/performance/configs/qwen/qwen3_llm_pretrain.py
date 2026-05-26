ed5866d26758dd5df1d024b0ea1d023202421abd
Malay Nagda <malayn@nvidia.com>
2026-05-26 19:26:29 +0530

    optimizer CG- qwen
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/qwen/qwen3_llm_pretrain.py
+++ scripts/performance/configs/qwen/qwen3_llm_pretrain.py
@@ -43,7 +43,7 @@ def set_qwen3_common_configs(cfg: ConfigContainer) -> None:
 
     cfg.model.moe_router_force_load_balancing = True  # required for token dropless
 
-    cfg.optimizer.optimizer_cuda_graph = False
+    cfg.optimizer.optimizer_cuda_graph = True
     cfg.checkpoint.save = None
 
 
